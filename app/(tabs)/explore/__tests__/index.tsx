6842502fecce6413c972b050301c40f5522e17c2
a7chax <akuarung83@gmail.com>
2026-04-08 21:44:29 +0700

    Add accordion testing to ExploreScreen tests

--- app/(tabs)/explore/__tests__/index.tsx
+++ app/(tabs)/explore/__tests__/index.tsx
@@ -1,5 +1,12 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import ExploreScreen from '..';
+
+jest.mock('@/components/parallax-scroll-view', () => {
+  const React = require('react');
+  const { View } = require('react-native');
+
+  return ({ children }: { children?: React.ReactNode }) => <View>{children}</View>;
+});
 
 jest.mock('@/components/external-link', () => {
   const React = require('react');
@@ -14,3 +21,16 @@ test('shows intro text', () => {
 
   expect(getByText('This app includes example code to help you get started.')).toBeTruthy();
 });
+
+
+
+describe("testing the accordion", () => {
+    test('should open the accordion', () => {
+        const { getByText } = render(<ExploreScreen />);
+        const accordion = getByText('File-based routing');
+        fireEvent.press(accordion);
+        expect(accordion).toHaveTextContent('File-based routing');
+        const content = getByText('This app has two screens: app/(tabs)/index.tsx and app/(tabs)/explore/index.tsx');
+        expect(content).toBeTruthy();
+    })
+})
